3e1af52b62998d90095e58eda5f5c406019e6c1e
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-08-23 14:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function getWeather() {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=27668eb3d23f0d2ee32008dcf1fba594')
+    .then(response => response.json())
+    .then(data => {
+        let weather = data['weather'][0]['description'];
+        document.getElementById('weatherData').innerHTML = 'Current weather in London: ' + weather;
+    })
+    .catch(error => console.error('Error:', error));
+}
